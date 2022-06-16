893ee85602b697be12b419ea9b906e563cce6dce
adrianolvti <adrianolvti@hotmail.com>
2022-06-15 21:19:32 -0300

    Início do projeto Cinevip

---
+++ src/main/java/br/edu/ifrs/restinga/cinevip/CinevipApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.restinga.cinevip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinevipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinevipApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifrs/restinga/cinevip/CinevipApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifrs.restinga.cinevip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinevipApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
