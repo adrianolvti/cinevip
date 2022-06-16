2c1f470518746338b87611b64a614f230dffa0c3
adrianolvti <adrianolvti@hotmail.com>
2022-06-15 21:50:53 -0300

    Criado estrutura de pacotes do projeto e iniciado primeira entidade

---
+++ src/main/java/br/edu/ifrs/restinga/cinevip/models/User.java
@@ -0,0 +1,41 @@
+package br.edu.ifrs.restinga.cinevip.models;
+
+public class User {
+
+    private long id;
+    private String name;
+    private String cpf;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
